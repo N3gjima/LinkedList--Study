fc30b9530529ac819c94f722a90626254bbeb7d8
Everton <76123738+N3gjima@users.noreply.github.com>
2022-11-25 14:31:52 -0300

    Update LinkedList- Python.py

--- LinkedList- Python.py
+++ LinkedList- Python.py
@@ -1,3 +1,5 @@
+
+
 class Node :
   def __init__(self, data):
     self.data = data
@@ -6,15 +8,28 @@ class Node :
 class LinkedList:
   def __init__(self):
     self.head = None
-    self.size = 0
+    self._size = 0
     
   def append(self, elem):
+    #Adiciona novos elementos a lista quando já há elementos
     if self.head:
       while(pointer.next):
         pointer = pointer.next
-      self.size = self.size + 1
+      pointer.next = Node(elem)
     
     
     else:
       self.head = Node(elem)
-
+    self._size =self._size + 1
+    
+    #retorna o tamanho da lista utilizando a função (len)
+  def __len__(self):
+    return self._size
+  #previsão de sobrecarga de operador
+  def __getitem__(self, index):
+    pointer = self.head
+    for i in range(index):
+      if pointer:
+        pointer = pointer. next
+      else:
+        raise IndexError("List index error out of range")
