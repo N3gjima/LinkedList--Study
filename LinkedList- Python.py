c961db12975ec89c32d502c1dcd4a679faddeae9
Everton <76123738+N3gjima@users.noreply.github.com>
2022-11-28 14:35:55 -0300

    Update LinkedList- Python.py

--- LinkedList- Python.py
+++ LinkedList- Python.py
@@ -1,35 +1,43 @@
-
-
-class Node :
-  def __init__(self, data):
-    self.data = data
-    self.next = None
-
+class Node:
+	def __init_(self, data):
+		self.data = data
+		self.next = None
 class LinkedList:
-  def __init__(self):
-    self.head = None
-    self._size = 0
-    
-  def append(self, elem):
-    #Adiciona novos elementos a lista quando já há elementos
-    if self.head:
-      while(pointer.next):
-        pointer = pointer.next
-      pointer.next = Node(elem)
-    
-    
-    else:
-      self.head = Node(elem)
-    self._size =self._size + 1
-    
-    #retorna o tamanho da lista utilizando a função (len)
-  def __len__(self):
-    return self._size
-  #previsão de sobrecarga de operador
-  def __getitem__(self, index):
-    pointer = self.head
-    for i in range(index):
-      if pointer:
-        pointer = pointer. next
-      else:
-        raise IndexError("List index error out of range")
+	def __init__(self):
+		self.head = None
+		self.size = 0
+
+	def append(self, elem):
+		if self.head:
+			while(pointer.next):
+				pointer = pointer.next
+			pointer.next = Node(elem)
+		else:
+			self.head = Node(elem)
+		self.size = self.size + 1
+
+	def __len__(self):
+		return self.size
+
+	def __getitem__(self, index):
+		pointer = self.head
+		for i in range(index):
+			if pointer:
+				pointer = pointer.next 
+			else:
+				raise IndexError("List index error out of range")	
+		if pointer: 
+			return pointer.data
+		raise IndexError("list index out of range")
+
+	def __setitem__(self, index, elem):
+		pointer = self.head
+		for i in range(index):
+			if pointer:
+				pointer = pointer.next
+			else:
+				raise IndexError("List index error out of range")
+		if pointer:
+			pointer.data = elem
+		else:
+			raise IndexError("List index error out of range")
